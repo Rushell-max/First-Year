2ef66ea304254b25581fd1c01ef9a6bfa0c20b37
Rushell-max <rzavalaga@unsa.edu.pe>
2021-06-16 12:11:30 -0500

    master

--- Laboratorio_ED_II/T2_Guía Matrices.cpp
+++ Laboratorio_ED_II/T2_Guía Matrices.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include<stdlib.h>
+#include <fstream>
 
 using namespace std;
 
-void leer(int Ma[][3], int fi,int co){
+void leer(int Ma[][4], int fi,int co){
 	cout<<"Ingrese valores: "<<endl;
 	for(int i=0; i<fi;i++){
 		for(int j=0; j<co; j++){
@@ -12,7 +13,18 @@ void leer(int Ma[][3], int fi,int co){
 	}
 }
 
-void mostrar(int Ma[][3], int fi,int co){
+void insertarValores(int Ma[][4], int fi,int co){
+    ifstream fichero;
+	fichero.open("<UBICACION>");
+
+	for(int i=0; i<fi; i++)
+		for(int j=0; j<co; j++){
+			fichero >> Ma[i][j];
+	}
+	fichero.close();
+}
+
+void mostrar(int Ma[][4], int fi,int co){
 	for(int i=0; i<fi;i++){
 		for(int j=0; j<co;j++)
 			cout<<Ma[i][j]<<" ";
@@ -20,7 +32,7 @@ void mostrar(int Ma[][3], int fi,int co){
 	}
 }
 
-int suma(int Ma[][3], int fi,int co){
+int suma(int Ma[][4], int fi,int co){
 	int s=0;
 	for(int i=0; i<fi;i++){
 		for(int j=0; j<co;j++){
@@ -30,13 +42,13 @@ int suma(int Ma[][3], int fi,int co){
 	return s;
 }
 
-float promedio (int Ma[][3], int fi,int co){
+float promedio (int Ma[][4], int fi,int co){
 	float supro;
 	supro=(float) suma(Ma,fi,co)/(fi*co);
 	return (supro);
 }
 
-int mayor(int Ma[][3], int fi,int co){
+int mayor(int Ma[][4], int fi,int co){
 	int may=Ma[0][0];
 	for(int i=0; i<fi;i++){
 		for(int j=0; j<co;j++){
@@ -48,8 +60,32 @@ int mayor(int Ma[][3], int fi,int co){
 	return may;
 }
 
+void eliminar(int Ma[][4], int fi, int co){
+	for(int i=0; i<fi;i++){
+		for(int j=0; j<co;j++){
+			Ma[i][j]=0;
+		}
+	}	
+}
+
+void duplicar(int Ma[][4], int fi, int co){
+	for(int i=0; i<fi;i++){
+		for(int j=0; j<co;j++){
+			Ma[i][j]*=2;
+		}
+	}	
+}
+
+float SumaDiagonalP(int Ma[][4], int fi, int co){
+    float suma = 0;
+    for (int i=0 ; i<fi ; i++){
+		suma+=Ma[i][i];
+    }
+    return suma;
+}
+
 int main(int argc, char *argv[]) {
-	const int fi=2, col=3;
+	const int fi=4, col=4;
 	int matriz[fi][col];
 	leer(matriz, fi, col);
 	cout<<"Los datos ingresados fueron"<<endl;
@@ -57,7 +93,7 @@ int main(int argc, char *argv[]) {
 	cout<<endl<<"La suma de los elementos ingresados es: "<<suma(matriz, fi, col)<<endl;
 	cout<<"El promedio de sus notas es : "<<promedio(matriz, fi, col)<<endl;
 	cout<<"el mayor de los elementos es: "<<mayor(matriz, fi, col)<<endl;
-    cout<<"La suma de la diagonal principal es: "<<sumaDiagPrinc(matriz, fi, col)<<endl;
+    cout<<"La suma de la diagonal principal es: "<<SumaDiagonalP(matriz, fi, col)<<endl;
 	system("PAUSE");
 	return 0;
 }
